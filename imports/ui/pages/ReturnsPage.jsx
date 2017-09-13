682a7d4706f0c178043d51f903d0bf2a80f12301
Brian Fajardo <bmf5@hotmail.com>
2017-09-12 20:00:32 -0400

    Brian taking over. Initial refactor: ES6.

--- imports/ui/pages/ReturnsPage.jsx
+++ imports/ui/pages/ReturnsPage.jsx
@@ -1,58 +1,43 @@
 // @flow
 
 // Framework
-import React, { Component } from "react";
-import { Meteor } from "meteor/meteor";
+import React, { Component } from "react"
+import { Meteor } from "meteor/meteor"
 
 // Components
-import { Alert, Row, Col } from "reactstrap";
-import Page from "../components/Page.jsx";
+import { Alert, Row, Col } from "reactstrap"
+import Page from "../components/Page.jsx"
 
 class ReturnsPage extends Component {
   constructor(props) {
-    super(props);
+    super(props)
     // Initialize State
-    this.initialState = {
+    // Calling super() sets "this" to refer to ReturnsPage when instantiated.
+    this.state = {
       lastOrder: null,
       error: null
-    };
-    this.state = this.initialState;
+    }
   }
 
-  componentWillMount() {
+  componentDidMount() {
+    // Best practice to set initial requests in componentDidMount
     Meteor.call("orders.getLastOrder", (error, response) => {
       if (error) {
-        this.setState(() => ({ error: error }));
+        this.setState({ error })
       }
-      this.setState(() => ({ lastOrder: response }));
-    });
+      this.setState({ lastOrder: response })
+    })
   }
 
   render() {
-    const { lastOrder, error } = this.state;
+    const { lastOrder, error } = this.state
+    console.log(lastOrder)
     return (
       <Page>
-        <Row>
-          <Col>
-            <Alert className="mt-3">
-              I would highly recommend understanding the structure of the order
-              object first and how it should relate to the designs.
-            </Alert>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            {JSON.stringify(lastOrder)}
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            {error}
-          </Col>
-        </Row>
+        <h1>React is awesome! 🤘</h1>
       </Page>
-    );
+    )
   }
 }
 
-export default ReturnsPage;
+export default ReturnsPage
