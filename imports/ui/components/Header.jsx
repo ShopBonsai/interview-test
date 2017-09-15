632abc88e26ceaecc27774270f8eab86b28ef7cd
Brian Fajardo <bmf5@hotmail.com>
2017-09-15 17:24:28 -0400

    validate prop types

--- imports/ui/components/Header.jsx
+++ imports/ui/components/Header.jsx
@@ -1,9 +1,23 @@
+// Framework
 import React from "react";
+
+// Libraries
+import PropTypes from "prop-types";
 
 const Header = ({ currentPage, totalPages, headerText }) =>
   <div>
-    <p>{currentPage} of {totalPages}</p>
-    <p>{headerText}</p>
+    <p>
+      {currentPage} of {totalPages}
+    </p>
+    <p>
+      {headerText}
+    </p>
   </div>;
+
+Header.propTypes = {
+  currentPage: PropTypes.number,
+  totalPages: PropTypes.number,
+  headerText: PropTypes.string
+};
 
 export default Header;
