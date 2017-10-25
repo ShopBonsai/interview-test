8e57db487ac740878cbed13621a161f6f8e6a756
Don Cooper <don_cooper@hotmail.com>
2017-10-25 12:58:46 -0400

    handleBuyProduct changed so alert shows name of product that will be added to cart. SweetAlert added to replace standard alert.

--- imports/ui/components/Product.jsx
+++ imports/ui/components/Product.jsx
@@ -5,8 +5,9 @@ import React, { PureComponent } from "react";
 import Button from "../components/Button.jsx";
 
 class Product extends PureComponent {
-  handleBuyProduct = () => {
-    alert("This button does nothing!");
+  handleBuyProduct = (e) => {
+    e.preventDefault();
+    sweetAlert(this.props.name + ' has been added to your cart.')
   };
 
   render() {
