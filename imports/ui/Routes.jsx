d3e2dd7077ef5f37c6245b4011343f092edbb36d
Lex Yehia <lex.yehia@icloud.com>
2017-11-24 02:19:23 -0500

    Wrapped Router base component with react-redux's Provider component

--- imports/ui/Routes.jsx
+++ imports/ui/Routes.jsx
@@ -1,21 +1,25 @@
 // Framework
 import React from "react";
+import { Provider } from "react-redux";
 import {
   BrowserRouter as Router,
   Route,
   browserHistory
 } from "react-router-dom";
+import Store from "./store";
 
 // Pages
 import Shop from "./pages/Shop.jsx";
 import Home from "./pages/Home.jsx";
 
 const Routes = () =>
-  <Router history={browserHistory}>
-    <div>
-      <Route exact path="/" component={Home} />
-      <Route path="/shop" component={Shop} />
-    </div>
-  </Router>;
+  <Provider store={Store}>
+    <Router history={browserHistory}>
+      <div>
+        <Route exact path="/" component={Home} />
+        <Route path="/shop" component={Shop} />
+      </div>
+    </Router>
+  </Provider>;
 
 export default Routes;
