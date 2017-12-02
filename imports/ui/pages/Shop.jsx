6710fa62407bdbb2efe30327bcf8c83d0377c834
Anton Yatseniuk <anton.yatseniuk@rolique.io>
2017-12-02 13:44:33 +0200

    added filters section, added sizes filters

--- imports/ui/pages/Shop.jsx
+++ imports/ui/pages/Shop.jsx
@@ -7,7 +7,7 @@ import { Form, FormGroup, Label, Input } from "reactstrap";
 import Page from "../components/Page.jsx";
 import Product from "../components/Product";
 
-const ALL_SIZES_VALUE = "all";
+const ALL_SIZES_VALUE = 'all';
 
 class Shop extends Component {
   constructor(props) {
@@ -20,6 +20,7 @@ class Shop extends Component {
   }
   
   _allProducts = [];
+  sizes = new Set();
 
   componentWillMount() {
     Meteor.call("merchants.getMerchants", (error, response) => {
@@ -27,6 +28,7 @@ class Shop extends Component {
         this.setState(() => ({ error: error }));
       } else {
         this._allProducts = response.reduce((acc, merchant) => [...acc, ...this.getProductsFromMerchant(merchant)], []);
+        this._allProducts.forEach(product => this.sizes.add(product.size));
 
         this.setState(() => ({
             merchants: response,
@@ -45,12 +47,43 @@ class Shop extends Component {
       brand: brands[belongsToBrand]
     }));
 
+  handleFilterSizeChange = ({target}) => {
+    const value = target.value;
+
+    if (value !== ALL_SIZES_VALUE) {
+      // filter products by selected size
+      this.setState({
+        products: this._allProducts.filter(product => product.size === value)
+      });
+    } else {
+      // show all products
+      this.setState({ products: [...this._allProducts] });
+    }
+  }
+
   render() {
     const { merchants, products, error } = this.state;
 
     return (
       <Page pageTitle="shop" history goBack={this.goBack}>
         <div className="shop-page">
+          {this.sizes.size > 0 &&
+            <div className="filters">
+              <h2 className="filters-title">Filters</h2>
+                <Form className="filters-form" inline>
+                  <Label for="sizesSelect">Select your size:&nbsp;</Label>
+
+                  <Input type="select" name="select" id="sizesSelect" onChange={this.handleFilterSizeChange}>
+                    <option value={ALL_SIZES_VALUE}>All</option>
+                    {Array.from(this.sizes).map(size =>
+                      <option value={size} key={size}>{size}</option>
+                    )}
+                  </Input>
+
+                </Form>
+            </div>
+          }
+
           {products.map(({ id, ...product }) =>
             <Product {...product} key={id} />
           )}
