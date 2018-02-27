b1bfab693e51bd6fcf13d7c0b4f72beb899356f5
Viven <viventhraarao@gmail.com>
2018-02-28 01:10:09 +0800

    Add loading icon

--- imports/ui/pages/Shop.jsx
+++ imports/ui/pages/Shop.jsx
@@ -3,52 +3,64 @@ import React, { Component } from "react";
 import { Meteor } from "meteor/meteor";
 
 // Components
-import { Alert, Row, Col } from "reactstrap";
 import Page from "../components/Page.jsx";
 import Product from "../components/Product";
+import Loading from "../components/Loading";
 
 class Shop extends Component {
   constructor(props) {
     super(props);
     this.state = {
       merchants: [],
-      error: null
+      error: null,
+      loading: false
     };
   }
 
   componentWillMount() {
+    this.setState(() => ({ loading: true }));
     Meteor.call("merchants.getMerchants", (error, response) => {
       if (error) {
         this.setState(() => ({ error: error }));
       } else {
         this.setState(() => ({ merchants: response }));
       }
+      this.setState(() => ({ loading: false }));
     });
   }
 
   goBack = () => this.props.history.push("/");
 
   render() {
-    const { merchants, error } = this.state;
+    const { merchants, loading } = this.state;
 
-    const getProductsFromMerchant = ({ products, brands }) =>
-      products.map(({ belongsToBrand, ...product }) => ({
-        ...product,
-        brand: brands[belongsToBrand]
-      }));
+    let content;
+    if (loading) {
+      content = <Loading />;
+    } else {
+      const getProductsFromMerchant = ({ products, brands }) =>
+        products.map(({ belongsToBrand, ...product }) => ({
+          ...product,
+          brand: brands[belongsToBrand]
+        }));
 
-    const products = merchants.reduce(
-      (acc, merchant) => [...acc, ...getProductsFromMerchant(merchant)],
-      []
-    );
+      const products = merchants.reduce(
+        (acc, merchant) => [...acc, ...getProductsFromMerchant(merchant)],
+        []
+      );
 
-    return (
-      <Page pageTitle="shop" history goBack={this.goBack}>
+      content = (
         <div className="shop-page">
           {products.map(({ id, ...product }) =>
             <Product {...product} key={id} />
-          )}
+          )};
         </div>
+      );
+    }
+
+    return (
+      <Page pageTitle="shop" history goBack={this.goBack}>
+        {content}
       </Page>
     );
   }
