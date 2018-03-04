17191d00998780e72fc351777da3e4f7f71084ef
grami <mohamed.grami@loyalcraft.com>
2018-03-04 01:26:50 +0100

    filter by merchant

--- imports/ui/pages/Shop.jsx
+++ imports/ui/pages/Shop.jsx
@@ -1,13 +1,15 @@
 // Framework
 import React, { Component } from "react";
 import { Meteor } from "meteor/meteor";
+import Icon from "react-icons-kit";
+import { circleDown } from "react-icons-kit/icomoon/circleDown";
 import {
   FormControl,
   FormGroup,
   HelpBlock,
   ControlLabel
 } from "react-bootstrap";
-import { filter } from "lodash";
+import { filter, take, indexOf, concat } from "lodash";
 
 // Components
 import Page from "../components/Page.jsx";
@@ -19,6 +21,8 @@ class Shop extends Component {
     super(props);
     this.state = {
       products: [],
+      merchants: [],
+      merchantsLastIndex: 10,
       shownProducts: [],
       cart: CartHelper.getCart(),
       error: null,
@@ -28,22 +32,32 @@ class Shop extends Component {
     };
   }
 
+  getProductsFromMerchant(merchant) {
+    return merchant.products.map(({ belongsToBrand, ...product }) => ({
+      ...product,
+      brand: merchant.brands[belongsToBrand]
+    }));
+  }
+
   componentWillMount() {
     Meteor.call("merchants.getMerchants", (error, response) => {
       if (error) {
         this.setState(() => ({ error: error }));
       } else {
-        const getProductsFromMerchant = ({ products, brands }) =>
-          products.map(({ belongsToBrand, ...product }) => ({
-            ...product,
-            brand: brands[belongsToBrand]
-          }));
         const products = response.reduce(
-          (acc, merchant) => [...acc, ...getProductsFromMerchant(merchant)],
-          []
-        );
+            (acc, merchant) => [
+              ...acc,
+              ...this.getProductsFromMerchant(merchant)
+            ],
+            []
+          ),
+          merchants = response.map(merchant => {
+            merchant.class = "";
+            return merchant;
+          });
+        this.setState({ merchants: merchants });
         this.setState({ products: products });
-        this.applyFilters();
+        this.applyKeyWordFilter(products);
       }
     });
   }
@@ -59,18 +73,26 @@ class Shop extends Component {
     CartHelper.saveCart(cart);
   };
 
-  applyFilters() {
+  showMore = () => {
+    let lastIndex = this.state.merchantsLastIndex + 10;
+    if (lastIndex >= this.state.merchants.length) {
+      lastIndex = this.state.merchants.length;
+    }
+    this.setState({ merchantsLastIndex: lastIndex });
+  };
+
+  applyKeyWordFilter(allProducts) {
     const search = this.state.search,
       products =
         this.state.search.keyword.trim() != ""
-          ? filter(this.state.products, product => {
+          ? filter(allProducts, product => {
               return (
                 product.name
                   .toUpperCase()
                   .indexOf(this.state.search.keyword.toUpperCase()) != -1
               );
             })
-          : this.state.products;
+          : allProducts;
     this.setState({ shownProducts: products });
     if (!products.length) {
       search.helpBlock = "No Product found for this search";
@@ -85,12 +107,31 @@ class Shop extends Component {
     this.setState({ search: search });
   }
 
+  applyMerchantsFilter() {
+    const products = [];
+    this.state.merchants.map(merchant => {
+      if (merchant.class === "active") {
+        products.push(...this.getProductsFromMerchant(merchant));
+      }
+    });
+    products.length
+      ? this.applyKeyWordFilter(products)
+      : this.applyKeyWordFilter(this.state.products);
+  }
+  selectMerchant = merchant => {
+    const merchants = this.state.merchants;
+    merchant.class = merchant.class === "active" ? "" : "active";
+    this.setState({ merchants: merchants });
+    this.applyMerchantsFilter();
+  };
+
   handleSearchChange = event => {
     const search = this.state.search;
     search.keyword = event.target.value;
     this.setState({ search: search });
-    this.applyFilters();
+    this.applyKeyWordFilter(this.state.products);
   };
+
   render() {
     return (
       <Page
@@ -100,7 +141,46 @@ class Shop extends Component {
         cart={this.state.cart}
       >
         <div className="row shop-page">
-          <div className="col-md-4" />
+          <div className="col-md-4">
+            <div className="row select-merchants">
+              <h1>Select Merchants:</h1>
+            </div>
+            {take(
+              this.state.merchants,
+              this.state.merchantsLastIndex
+            ).map(merchant =>
+              <div
+                onClick={() => this.selectMerchant(merchant)}
+                className={"row merchant " + merchant.class}
+                key={merchant._id}
+              >
+                <div className="col-2">
+                  <img src={merchant.logo} />
+                </div>
+                <div className="col-8">
+                  <p>
+                    {merchant.merchant}
+                  </p>
+                  <span>
+                    {merchant.address}
+                  </span>
+                  <br />
+                  <span>
+                    {merchant.phone}
+                  </span>
+                </div>
+                <div className="col-2" />
+              </div>
+            )}
+            <div className="show-more">
+              {this.state.merchantsLastIndex < this.state.merchants.length
+                ? <div>
+                    <h2 onClick={this.showMore}>SHOW MORE</h2>
+                    <Icon icon={circleDown} />
+                  </div>
+                : <h2>Nothing more to show</h2>}
+            </div>
+          </div>
           <div className="col-md-8">
             <form className="row filter-key">
               <FormGroup
