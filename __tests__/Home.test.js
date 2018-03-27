eb0f7a3a09cc927d80538fdf1e49dd00eac3734f
Mary Shrives <mshrives@alumni.yorku.ca>
2018-03-27 16:11:46 -0400

    finished with tests

--- __tests__/Home.test.js
+++ __tests__/Home.test.js
@@ -9,3 +9,4 @@ describe("Home", () => {
     ReactDom.render(<Home />, div);
   });
 });
+
--- __tests__/Shop.test.js
+++ __tests__/Shop.test.js
@@ -1,7 +1,23 @@
 import data from "../imports/startup/server/mockMerchantData.json";
-
+//displaying all available items
 const numItems = data.length;
 
 test("Number of items = 198", () => {
   expect(numItems).toBe(198);
+});
+
+//check that item is in stock
+const productQuantity = data[0].products[0].quantity;
+
+test("Number of items to be greater than 0", () => {
+  expect(productQuantity).toBeGreaterThan(0);
+});
+
+//Objects
+test("The first item to have a property of products", () => {
+  expect(data[0]).toHaveProperty("products");
+});
+
+test("The second item to have the merchant name STROZEN", () => {
+  expect(data[1]).toHaveProperty("merchant", "STROZEN");
 });
