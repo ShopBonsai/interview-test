cff48506f0733862dcd9508bdeba69f07f2a8a18
egomatsushita <eduardo.go.rj@gmail.com>
2018-03-30 20:01:22 -0400

    Add promise to handle orders request

--- imports/ui/pages/Shop.jsx
+++ imports/ui/pages/Shop.jsx
@@ -38,21 +38,45 @@ class Shop extends Component {
     this.props.history.push("/cart");
   }
 
-  componentDidMount() {
+  createNewOrder() {
     const items = this.state.items;
     const user = this.state.user;
     const email = this.state.user.email;
-    
+
     Meteor.call("orders.addOrder", items, user, email, (error, response) => {
       if (error) {
-        this.setState(() => ({ error: error} ));
+        this.setState(() => ({ error: error }));
       } else {
-        this.setState(() => ({ orders: response }));
+        this.setState(() => ({ orders: this.state.concat(response) }));
       }
     });
   }
 
   componentWillMount() {
+    const email = this.state.user.email;
+    
+    let promise = new Promise((resolve, reject) => {
+      Meteor.call("orders.getOrdersByEmail", { email: email }, (error, response) => {
+        if (error) {
+          this.setState(() => ({ error: error }));
+          reject(error);
+        } else {
+          this.setState(() => ({ orders: response }));
+          resolve(response);
+        }
+      })
+    });
+
+    promise
+      .then(() => {
+        let orders = this.state.orders;
+        if (orders.length) {
+          console.log('ok passei ', this.state.orders.length)
+        } else{
+          console.log('error no promise')
+        }})
+      .catch((error) => console.log('catch ', error));
+
     Meteor.call("merchants.getMerchants", (error, response) => {
       if (error) {
         this.setState(() => ({ error: error }));
@@ -60,14 +84,13 @@ class Shop extends Component {
         this.setState(() => ({ merchants: response }));
       }
     });
-
   }
 
   goBack = () => this.props.history.push("/");
 
   render() {
-    const { merchants, error } = this.state; //+++++++++++++
-
+    const { merchants, error, orders, items, user } = this.state; //+++++++++++++
+    
     const getProductsFromMerchant = ({ products, brands }) =>
       products.map(({ belongsToBrand, ...product }) => ({
         ...product,
