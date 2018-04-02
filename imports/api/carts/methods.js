de69a74e961d6655015e542aac56d752e34c6b5e
Vasiliy Klimkin <vasily.klimkin@gmail.com>
2018-04-01 20:54:33 -0400

    linting fixes.

--- imports/api/carts/methods.js
+++ imports/api/carts/methods.js
@@ -6,43 +6,37 @@ import { Meteor } from "meteor/meteor";
 // Collections
 import { Carts } from "./collection";
 
-
 export const createCart = () => {
   let id;
   try {
-    id = Carts.insert({products: []})
+    id = Carts.insert({ products: [] });
   } catch (error) {
     throw new Meteor.Error("Error: ", error);
   }
   return id;
 };
 
-export const getCart = (cartId) => {
+export const getCart = cartId => {
   let cart;
   try {
-    cart = Carts.find({_id: cartId}).fetch();
-
+    cart = Carts.find({ _id: cartId }).fetch();
   } catch (error) {
     throw new Meteor.Error("Error: ", error);
   }
   return cart;
 };
 
-export const addProductToCart = (params) => {
-  Carts.update(
-    {_id: params.cartId},
-    {$push: { products : params.product}}
-  );
+export const addProductToCart = params => {
+  Carts.update({ _id: params.cartId }, { $push: { products: params.product } });
 };
 
-export const deleteCart = (cartId) => {
+export const deleteCart = cartId => {
   try {
-    Carts.remove({ _id: cartId});
+    Carts.remove({ _id: cartId });
   } catch (error) {
     throw new Meteor.Error("Error: ", error);
   }
-}
-
+};
 
 Meteor.methods({
   "carts.createCart": createCart,
