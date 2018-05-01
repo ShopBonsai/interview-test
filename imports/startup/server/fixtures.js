005b0d97d6aebad6fc8445e20e0c95c5eac372de
Colin Nebocat <colin@nebocat.ca>
2018-04-30 22:07:11 -0700

    Add SeedHelper, seeding complete for categories, profileTypes, orderStatus

--- imports/startup/server/fixtures.js
+++ imports/startup/server/fixtures.js
@@ -10,10 +10,48 @@ import Products from "../../api/products/collection";
 import Merchants from "../../api/merchants/collection";
 import Customers from "../../api/customers/collection";
 import Orders from "../../api/orders/collection";
-
 import mockMerchantData from "./mockMerchantData.json";
+import seedData from "./seedData";
+import SeedHelper from "./seedHelper";
 
 // startup function
-Meteor.startup(() => {
-  console.log('MOCK DATA LENGTH:'.yellow, mockMerchantData.length);
+Meteor.startup(async () => {
+  // cheeck seed data length
+  console.log('Mock Data Items:'.yellow, mockMerchantData.length);
+  // collect collections
+  const collections = [
+    Brands,
+    Categories,
+    ProfileTypes,
+    OrderStatus,
+    Products,
+    Merchants,
+    Customers,
+    Orders
+  ];
+  // instantiate new SeedHelper
+  const helper = new SeedHelper;
+  // clear collections
+  helper.clearCollections(collections);
+
+  // build categores
+  const categories = Object.keys(seedData.categories)
+    .map(item => ({ name: item }));
+  // console.log(categories);
+  const seededCategories = await helper.insertDocs(collections[1], categories);
+  // console.log("Seeded Categories:".yellow, seededCategories);
+
+  // build profileTypes
+  const profileTypes = Object.values(seedData.profileTypes)
+    .map(item => ({ name: item }));
+    // console.log(profileTypes);
+  const seededProfileTypes = await helper.insertDocs(collections[2], profileTypes);
+  // console.log("Seeded Profile Types:".yellow, seededProfileTypes);
+
+  // build orderStatus
+  const orderStatus = Object.values(seedData.orderStatus)
+    .map(item => ({ name: item }));
+    // console.log(orderStatus);
+  const seededOrderStatus = await helper.insertDocs(collections[3], orderStatus);
+  // console.log("Seeded Order Status:".yellow, seededOrderStatus);
 });
---
+++ imports/startup/server/seedData.js
@@ -0,0 +1,96 @@
+const seedData = {
+  categories: {
+    tops: [
+      "shirt",
+      "polo shirt",
+      "hoody",
+      "top",
+      "sweatshirt",
+      "t-shirt",
+      "cardigan",
+      "fleece",
+      "blouse"
+    ],
+    bottoms: [
+      "kilt",
+      "skirt",
+      "shorts",
+      "jeans",
+      "cargos",
+      "tights"
+    ],
+    underwear: [
+      "thong",
+      "bra",
+      "knickers",
+      "lingerie",
+      "boxers",
+      "stockings",
+      "corset",
+      "briefs",
+      "underwear",
+      "pajamas",
+      "nightgown",
+      "camisole",
+      "robe"
+    ],
+    footwear: [
+      "shoes",
+      "sandals",
+      "boots",
+      "socks",
+      "slippers"
+    ],
+    accessories: [
+      "tie",
+      "belt",
+      "cufflinks",
+      "cravat",
+      "sunglasses",
+      "bow tie",
+      "shawl",
+      "scarf",
+      "hat",
+      "gloves",
+      "cummerbund"
+    ],
+    suits: [
+      "suit",
+      "jogging suit",
+      "overalls",
+      "gown",
+      "dress"
+    ],
+    outerwear: [
+      "coat",
+      "jacket",
+      "dinner jacket",
+      "blazer",
+      "waistcoat",
+      "tracksuit",
+      "poncho"
+    ],
+    beach: [
+      "sarong",
+      "swimwear",
+      "bikini",
+      "swimming shorts",
+      "tankini"
+    ]
+  },
+  profileTypes: [
+    "merchant",
+    "customer",
+    "admin"
+  ],
+  orderStatus: [
+    "cart",
+    "paid",
+    "collecting",
+    "shipped",
+    "received"
+  ]
+};
+
+// export module
+export default seedData;
