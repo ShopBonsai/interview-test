0192090cc705b80ee11492e2f696c17f5a83763a
Kirill Nikolaev <kirill.nikolaev@arrivalsdepartures.com>
2018-05-01 11:00:19 -0400

    Add Thank you page route

--- imports/ui/Routes.jsx
+++ imports/ui/Routes.jsx
@@ -9,12 +9,14 @@ import {
 // Pages
 import Shop from "./pages/Shop.jsx";
 import Home from "./pages/Home.jsx";
+import ThankYou from "./pages/ThankYou.jsx";
 
 const Routes = () =>
   <Router history={browserHistory}>
     <div>
       <Route exact path="/" component={Home} />
-      <Route path="/shop" component={Shop} />
+      <Route exact path="/shop" component={Shop} />
+      <Route exact path="/shop/thank-you" component={ThankYou} />
     </div>
   </Router>;
 
