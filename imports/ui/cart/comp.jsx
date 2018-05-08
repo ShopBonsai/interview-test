fb0d18ce1633a107e0b919a372fbcc6ba9436d41
Colin Nebocat <colin@nebocat.ca>
2018-05-07 21:33:49 -0700

    Adjust header image coverage for mobile

--- imports/ui/cart/comp.jsx
+++ imports/ui/cart/comp.jsx
@@ -9,15 +9,15 @@ const CartComp = ({ ...props }) =>
   <Container fluid id="cart-page">
     <NavHeader
       heading="Bonsai SmartCart"
-      subtitle="Look sharp and stay smart with your Bonsai SmartCart"
+      subtitle="Look sharp and stay smart with your own Bonsai SmartCart"
       id="cart-head"
     />
     <Container>
       <Row>
         <Col>
-          <h1>
+          <h2>
             <FontAwesomeIcon icon="shopping-cart" />Cart
-          </h1>
+          </h2>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
           </p>
