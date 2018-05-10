2a689807ccb27cf9f553009611562411c6cd326f
Colin Nebocat <colin@nebocat.ca>
2018-05-10 16:32:33 -0700

    Layout collecitons stats table

--- imports/ui/admin/comp.jsx
+++ imports/ui/admin/comp.jsx
@@ -1,25 +1,29 @@
 // import modules
 import React from "react";
-import { Container, Row, Col, Table } from "reactstrap";
+import { Container, Row, Col, Jumbotron, Table } from "reactstrap";
+import Stats from "./stats.jsx";
 
 // define component
 const AdminComp = ({ ...props }) =>
-  <Container>
-    <Row>
-      <Col>
-        <Table>
-          <thead>
-            <tr>
-              <th>Collection</th>
-              <th>Items</th>
-            </tr>
-          </thead>
-          <tbody>
-          </tbody>
-        </Table>
-      </Col>
-    </Row>
-  </Container>;
+  <div id="admin-page">
+    <Container fluid>
+      <Row>
+        <Col>
+          <Jumbotron fluid>
+            <h1>Bonsai Shop Admin</h1>
+          </Jumbotron>
+        </Col>
+      </Row>
+    </Container>
+    <Container>
+      <Row>
+        <Col id="stats">
+          <h2>Collection Stats</h2>
+          <Stats props={props} />
+        </Col>
+      </Row>
+    </Container>
+  </div>;
 
 // export component
 export default AdminComp;
---
+++ imports/ui/admin/stats.jsx
@@ -0,0 +1,87 @@
+// import modules
+import React from "react";
+import { Container, Row, Col, Jumbotron, Table } from "reactstrap";
+
+// define component
+const Stats = ({ ...props }) => {
+  const {
+    brands,
+    categories,
+    customers,
+    merchants,
+    orderStatus,
+    orders,
+    products,
+    profileTypes,
+    users
+  } = props.props;
+  return (
+    <Table responsive>
+      <thead>
+        <tr>
+          <th>Collection</th>
+          <th>Items</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Brands</td>
+          <td>
+            {brands.length}
+          </td>
+        </tr>
+        <tr>
+          <td>Categories</td>
+          <td>
+            {categories.length}
+          </td>
+        </tr>
+        <tr>
+          <td>Customers</td>
+          <td>
+            {customers.length}
+          </td>
+        </tr>
+        <tr>
+          <td>Merchants</td>
+          <td>
+            {merchants.length}
+          </td>
+        </tr>
+        <tr>
+          <td>Orders</td>
+          <td>
+            {orders.length}
+          </td>
+        </tr>
+        <tr>
+          <td>Order Status</td>
+          <td>
+            {orderStatus.length}
+          </td>
+        </tr>
+        <tr>
+          <td>Products</td>
+          <td>
+            {products.length}
+          </td>
+        </tr>
+        <tr>
+          <td>Profile Types</td>
+          <td>
+            {profileTypes.length}
+          </td>
+        </tr>
+        <tr>
+          <td>Users</td>
+          <td>
+            {users.length}
+          </td>
+        </tr>
+      </tbody>
+    </Table>
+  );
+};
+
+// export component
+export default Stats;
